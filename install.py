dd4710c3ee2833bcdd5e737e6e205888c4f5c441
Daniel Schäfer <schaefer.dev@gmail.com>
2019-08-01 15:26:04 +0200

    added support for icons to be used + changed internal layout identifier

--- install.py
+++ install.py
@@ -5,18 +5,18 @@ import platform
 import sys
 import subprocess
 
-filename = "CSLayout.keylayout"
+layout_filename = "CSLayout.keylayout"
+icon_filename = "CSLayout.icns"
 
 if platform.system() == "Darwin":  # check if Mac
   target = '/Library/Keyboard\\ Layouts/'
-  command = f"sudo cp {filename} {target}"
+  command = f"sudo cp {layout_filename} {target}"
+  command2 = f"sudo cp {icon_filename} {target}"
   if os.geteuid() != 0:
     print("We need to copy the layout file to `/Library/Keyboard\ Layouts/`. This requires root privileges, so your OS will ask you for your password.")
-    print("If you feel uneasy granting root privileges to some dude on the Internet, feel free to copy the file yourself:")
-    print(f"Simply run: `sudo cp {filename} {target}`.")
     print("After copying, you will need to open Apple > System Preferences > Keyboard > Input Sources, click the little \"+\", go to \"others\" and select the CSKeyboardLayout. Voilà!")
-    print("Note: if there is already an open System Preferences window, you need to close and re-open it.")
     os.system(command)
+    os.system(command2)
 else: 
   print("Sorry, We currently only support Mac. You need to install the layout manually.")
 
